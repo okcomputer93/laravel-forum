f7538f441babf6659c172e78ccd911807d0db008
Omar J <okcomputer2393@gmail.com>
2021-07-14 17:25:48 -0500

    first commit including first test

---
+++ app/Models/Thread.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Thread extends Model
+{
+    use HasFactory;
+
+    public function path()
+    {
+        return route('threads.show', $this);
+    }
+}
